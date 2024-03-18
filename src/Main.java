6923e95d7e1b9b7233db4541f0c69a32a0141815
Talgat Shaiken <talgat.shaiken@yandex.kz>
2024-03-18 20:28:00 +0500

    next1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number1 = 10;
+        int number2 = 20;
+        if (number1 < number2) {
+            System.out.println("Ехала");
+        }
     }
 }
