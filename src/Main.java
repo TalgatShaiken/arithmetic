e55c36e2ba4746c22bf873fcda16eab01e64c479
Talgat Shaiken <talgat.shaiken@yandex.kz>
2024-03-19 20:47:05 +0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        int number1 = 10;
+        int number1 = 30;
         int number2 = 20;
         if (number1 < number2) {
             System.out.println("Ехала");
         }
+        System.out.println(new Date());
     }
 }
