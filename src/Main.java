c2a39b52541ed32467c76d624ccbfc5f01aa7180
Talgat Shaiken <talgat.shaiken@yandex.kz>
2024-03-25 13:32:14 +0500

    25.03.2024

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int number1 = 30;
-        int number2 = 20;
-        if (number1 < number2) {
-            System.out.println("Ехала");
-        }
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
         System.out.println(new Date());
+        for (;;) {
+            System.out.println("Что нужно сделать?");
+            System.out.println("Введите первой число");
+            int number1 = new Scanner(System.in).nextInt();
+            System.out.println("Введите второе число");
+            int number2 = new Scanner(System.in).nextInt();
+            int result = number1 + number2;
+            System.out.println(result);
+        }
+
     }
 }
