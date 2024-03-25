ed51764271cf21bc8c6c0ad606e36afe815f373c
Talgat Shaiken <talgat.shaiken@yandex.kz>
2024-03-25 20:02:48 +0500

    created the arithmetic class

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for (;;) {
-            System.out.println("Введите первое число: ");
-            int number1 = new Scanner(System.in).nextInt();
-            System.out.println("Введите второе число: ");
-            int number2 = new Scanner(System.in).nextInt();
-            int result = number1 + number2;
-            System.out.println("Сумма ваших чисел: " + result);
+        System.out.println();
+
+    }
+
+    public class arithmetic {
+        private int num1 = new Scanner(System.in).nextInt();;
+        private int num2 = new Scanner(System.in).nextInt();;
+
+        public arithmetic(int num1, int num2) {
+            this.num1 = new Scanner(System.in).nextInt();
+            this.num2 = new Scanner(System.in).nextInt();
+        }
+
+        public int sum() {
+            return num1 + num2;
+        }
+
+        public int product() {
+            return num1 * num2;
+        }
+
+        public int max() {
+            return Math.max(num1, num2);
+        }
+
+        public int min() {
+            return Math.min(num1, num2);
         }
     }
 }
