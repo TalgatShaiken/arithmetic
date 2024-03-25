39c436d9970ccc7bcb275de93f218c0925371425
Talgat Shaiken <talgat.shaiken@yandex.kz>
2024-03-25 14:05:53 +0500

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        for (;;) {
+            System.out.println("Введите первое число: ");
+            int number1 = new Scanner(System.in).nextInt();
+            System.out.println("Введите второе число: ");
+            int number2 = new Scanner(System.in).nextInt();
+            int result = number1 + number2;
+            System.out.println("Сумма ваших чисел: " + result);
         }
-        System.out.printf("001");
     }
 }
